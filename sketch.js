1ce02a9799f77db1b95f5f4ad277358c02a632d0
RohitGarudadri <rohit.garudadri@mail.mcgill.ca>
2020-03-20 20:44:23 -0400

    Images

--- sketch.js
+++ sketch.js
@@ -1,8 +1,20 @@
 function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    background(220);
-    square(100,100,100); 
-  }
+  createCanvas(1000, 1000);
+}
+imgDB = loadImage("assets/darkblue_sq.png");
+imgG = loadImage("assets/green_sq.png");
+imgLB = loadImage("assets/lightblue_sq.png");
+imgO = loadImage("assets/orange_sq.png");
+imgP = loadImage("assets/pink_sq.png");
+imgPU = loadImage("assets/purple_sq.png");
+imgY = loadImage("assets/yellow_sq.png");
+GameBoard = loadImage("assets/TetrisBoard.png");
+
+function draw() {
+  background(220); // rgb value
+  // square(100, 100, 100); //top, left corner, and size
+  image(GameBoard, 0, 0);
+  image(imgY, 0, 0);
+  image(imgY, 45, 0);
+  image(imgDB, 90, 90);
+}
