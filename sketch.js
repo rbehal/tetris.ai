45f903a071499d87a65f38a89ab333c4f0f0c1dd
rbehal <rahulbehal01@hotmail.com>
2020-03-25 23:32:42 -0400

    JSDocced sketch.JS

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
-var gameboard;
-var activeShape;
+var gameboard; // Holds the one and only gameboard object for the game.
+var activeShape; // The current activeShape (falling shape on the board)
 var seconds = 0;
 var frames = 0;
-var rand_shapes = [];
+var rand_shapes = []; // Array of integers that correspond to random shapes. See randomShape() for more info.
 
+// Block image variables to be used on the grid.
 var darkblue_sq;
 var green_sq;
 var lightblue_sq;
@@ -12,6 +13,7 @@ var pink_sq;
 var purple_sq;
 var yellow_sq;
 
+// Full piece image variables to be used in the HOLD and NEXT sections.
 var T_img;
 var L_img;
 var J_img;
@@ -20,71 +22,73 @@ var S_img;
 var Line_img;
 var Box_img;
 
-var hold = [];
-
-var x;
-var y;
+var hold = []; // hold[0] represents the image object in hold. hold[1] represents the # corresponding the shape.
 
 
-
+/**
+ * Setup function runs once when the page is loaded. Loads images, creates canvas and gameboard, and chooses the first few shapes.
+ */
 function setup() {
-  // Loading images
   preloadImages();
+
   createCanvas(650, 750);
+
   gameboard = new Gameboard();
-
-
 
   for (let i = 0; i < 5; i++) {
     rand_shapes.push(randomNumber());
   }
-
 }
 
+/**
+ * Draw function runs 60 times per seconds. Serves to render and animate everything on the screen.
+ */
 function draw() {
   background(255);
 
-  x = mouseX;
-  y = mouseY;
-
   imageMode(CORNER);
-  image(gameboard.gameboard_img, 20, 20);
+  image(gameboard.gameboard_img, 20, 20); // Displays gameboard background.
 
-  gameboard.displayNext();
+  gameboard.displayNext(); // Displays all of the pieces coming up next.
+
   if (hold.length != 0) {
-    image(hold[0], 75, 160);
-  }
-  // fill(255);
-  // text(mouseX.toString(),100,150);
-  // text (mouseY.toString(),100,175);
-  if (gameboard.activeShape.shape == false) {
-    randomShape(rand_shapes.shift());
-    rand_shapes.push(randomNumber());
+    image(hold[0], 75, 160); // Displays the hold image.
   }
 
-  gameboard.display();
+  if (gameboard.activeShape.shape == false) { // Generates new random shape if there is none on the board.
+    randomShape(rand_shapes.shift()); // dequeue
+    rand_shapes.push(randomNumber()); // enqueue
+  }
 
-  // Timer stuff
+  gameboard.display(); // Renders every active block on the grid.
+
+  // Timer
   frames++;
   if (frames == 60) {
     seconds++;
     frames = 0;
-    this.gameboard.moveDown();
+    // TODO: Replace with level mechanism that adjusts the rate of moving down
+    this.gameboard.moveDown(); // Shifts gameboard down once per second
   }
   fill(255);
-  text(seconds.toString(), width / 2 - 5, 60);
-
+  text(seconds.toString(), width / 2 - 5, 60); // Renders seconds text at the top
 }
 
+/**
+ * Runs once when the mouse is clicked.
+ */
 function mouseClicked() { // For testing
   // this.gameboard.moveDown();
   // this.gameboard.moveRight();
   // gameboard.rotate(gameboard.activeShape);
   // gameboard.clearLine();
   // noLoop();
-  gameboard.deleteHold();
+  // gameboard.deleteHold();
 }
 
+/**
+ * Runs any time a key is pressed.
+ */
 function keyPressed() {
   if (keyCode === LEFT_ARROW) {
     gameboard.moveLeft();
@@ -101,14 +105,17 @@ function keyPressed() {
   if (keyCode == 32) { // ASCII for spacebar
     let swtch = true;
     while (swtch) {
-      swtch = gameboard.moveDown();
+      swtch = gameboard.moveDown(); // Moves down until it can't anymore
     }
   }
-  if (keyCode == 16) {
+  if (keyCode == 16) { // ASCII for shift
     gameboard.hold();
   }
 }
 
+/**
+ * Returns a number that is a random roll of the die from 0 to 7 where 0 to 6 correspond to a shape and 7 is a reroll. 
+ */
 function randomNumber() {
   let rand_num = Math.floor(Math.random() * 8);
   if (rand_num == 7) {
@@ -119,6 +126,10 @@ function randomNumber() {
   }
 }
 
+/**
+ * Spawns a random shape based on the given integer.
+ * @param {int} rand_shape 
+ */
 function randomShape(rand_shape) {
   activeShape = new Shapes();
   if (rand_shape == 0) {
@@ -138,6 +149,9 @@ function randomShape(rand_shape) {
   } 
 }
 
+/**
+ * Loads all of the images.
+ */
 function preloadImages() {
   darkblue_sq = loadImage("assets/darkblue_sq.png", img => {
     img.resize(32, 32);
